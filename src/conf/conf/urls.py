b3ab9f3d3cca7c8b863dc0e5cf3042fcbb3ba0dd
mowbish <mobin.clash.ma@gmail.com>
2022-10-29 16:13:15 +0330

    feat: Added router for base url module

--- src/conf/conf/urls.py
+++ src/conf/conf/urls.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from accounts.api import urls as accounts_router
+from orders.api import urls as orders_router
+from products.api import urls as products_router
 
 router = DefaultRouter()
+
+router.registry.extend(accounts_router.registry)
+router.registry.extend(orders_router.registry)
+router.registry.extend(products_router.registry)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
