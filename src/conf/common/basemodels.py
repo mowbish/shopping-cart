ccdf5bac5ba9fff670d1b049df77cf7d8f3d16cc
mowbish <mobin.clash.ma@gmail.com>
2022-10-29 16:18:48 +0330

    feat: Created common file and added base model and base manager

---
+++ src/conf/common/basemodels.py
@@ -0,0 +1,22 @@
+import uuid
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class BaseManager(models.Manager):
+
+    def get_queryset(self):
+        return super().get_queryset().filter(is_deleted=False)
+
+class BaseModel(models.Model):
+    """
+    Base model for inheritence for another models.
+    """
+    id = models.UUIDField(_("id"), primary_key=True, default=uuid.uuid4, editable=False)
+    updated_at = models.DateTimeField(_("updated_at"), auto_now=True)
+    created_at = models.DateTimeField(_("created_at"), auto_now_add=True)
+    is_deleted = models.BooleanField(_("is_deleted"), default=False, editable=False)
+    objects = BaseManager()
+
+    class Meta:
+        abstract = True
