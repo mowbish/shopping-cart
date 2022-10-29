baf4eecd70a2e7f581c4bf51822867b0db917a3e
mowbish <mobin.clash.ma@gmail.com>
2022-10-29 17:19:33 +0330

    fix: Changed unused imports in view

--- src/conf/accounts/api/views.py
+++ src/conf/accounts/api/views.py
@@ -1,9 +1,5 @@
-from rest_framework import viewsets, mixins, filters
-from rest_framework.permissions import IsAuthenticated
-from .serializers import (SignUpSerializer, ChangePasswordSerializer, )
-from accounts.models import Customer
-from rest_framework import status
-from rest_framework.response import Response
+from rest_framework import viewsets, mixins
+from .serializers import (SignUpSerializer, )
 
 
 class SignUpModelViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
