25408937e60ecd2d41891d8b2ce8333d69c36408
mowbish <mobin.clash.ma@gmail.com>
2022-10-29 16:29:26 +0330

    feat: Created Customer model

--- src/conf/accounts/models.py
+++ src/conf/accounts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+from common.utils import phone_number_regex
 
-# Create your models here.
+
+class Customer(AbstractUser):
+    """
+        Becouse we dont inherit from the base model,so
+        we use the created, and updated fields in this class.
+    """
+
+    phone_number = models.CharField(
+        validators=[phone_number_regex], max_length=16, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = _("customers")
+        verbose_name = _('Customer')
+        verbose_name_plural = _('Customers')
+
+    def __str__(self):
+        return f'{self.first_name}{self.last_name}'
