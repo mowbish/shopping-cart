6985849d5bf6a81d0248834f3482d1256123c259
mowbish <mobin.clash.ma@gmail.com>
2022-10-29 17:26:18 +0330

    feat: Added serializer for customer signup

--- src/conf/accounts/api/serializers.py
+++ src/conf/accounts/api/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from accounts.models import Customer
+
+
 class SignUpSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Customer
-        fields = (phone_number)
+        fields = ("username", "first_name", "last_name", "email", "password")
