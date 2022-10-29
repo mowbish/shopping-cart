99895253d3c72008175ad8a4b62e476fea83adcc
mowbish <mobin.clash.ma@gmail.com>
2022-10-29 17:58:16 +0330

    fix: Added JWT for auth

--- src/conf/conf/urls.py
+++ src/conf/conf/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from accounts.api.urls import router as accounts_router
 #from orders.urls import router as orders_router
 #from products.urls import router as products_router
-
+from rest_framework_simplejwt import views as jwt_views
 router = DefaultRouter()
 
 router.registry.extend(accounts_router.registry)
@@ -15,4 +15,7 @@ router.registry.extend(accounts_router.registry)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+
 ]
