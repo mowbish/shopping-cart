262ed5271fdcccdbd863751ec46b0aad912a610c
mowbish <mobin.clash.ma@gmail.com>
2022-10-29 17:45:57 +0330

    fix: Added JWT for auth

--- src/conf/conf/settings.py
+++ src/conf/conf/settings.py
@@ -75,6 +75,13 @@ DATABASES = {
 }
 
 
+#DRF
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
