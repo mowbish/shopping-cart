301a1b44d8fb2c92c6e1d4ff239f34e7f3c8e42b
mowbish <mobin.clash.ma@gmail.com>
2022-11-04 02:02:20 +0330

    feat: Added new models and fields

--- src/conf/accounts/models.py
+++ src/conf/accounts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
+from conf import settings
+from common.basemodels import BaseModel
 
 
 class Customer(AbstractUser):
@@ -19,3 +21,28 @@ class Customer(AbstractUser):
 
     def __str__(self):
         return f'{self.first_name}{self.last_name}'
+
+
+class Address(BaseModel):
+    HOME = 'home'
+    OFFICE = 'office'
+    OTHER = 'other'
+    ADDRESS_TYPE = [(HOME, _('Home')), (OFFICE, _('Office')), (OTHER, _('Other')), ]
+
+    # Users can have one or many addresses
+    customer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    address = models.TextField(_("Address"))
+    country = models.CharField(_("Country"), max_length=30, )
+    state = models.CharField(_("State"), max_length=150, )
+    city = models.CharField(_("City"), max_length=150, )
+    post_code = models.CharField(_("Post_code"), max_length=12, )
+    address_type = models.CharField(_("Address_type"), max_length=6, choices=ADDRESS_TYPE, default=HOME)
+
+
+    class Meta:
+        db_table = _("addresses")
+        verbose_name = _("Address")
+        verbose_name_plural = _("Addresses")
+
+    def __str__(self):
+        return f'{self.address}'
--- src/conf/common/basemodels.py
+++ src/conf/common/basemodels.py
@@ -13,8 +13,8 @@ class BaseModel(models.Model):
     Base model for inheritence for another models.
     """
     id = models.UUIDField(_("id"), primary_key=True, default=uuid.uuid4, editable=False)
-    updated_at = models.DateTimeField(_("updated_at"), auto_now=True)
     created_at = models.DateTimeField(_("created_at"), auto_now_add=True)
+    updated_at = models.DateTimeField(_("updated_at"), auto_now=True)
     is_deleted = models.BooleanField(_("is_deleted"), default=False, editable=False)
     objects = BaseManager()
 
