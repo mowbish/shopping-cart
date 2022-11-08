d1ce6de68e3a177f77d819b6d8b472365f7d331d
mowbish <mobin.clash.ma@gmail.com>
2022-11-08 22:31:50 +0330

    feat: Added RetriveSignUpModelSerializer -> next step: class for update user and view of them

--- src/conf/accounts/api/serializers.py
+++ src/conf/accounts/api/serializers.py
@@ -2,8 +2,16 @@ from rest_framework import serializers
 from accounts.models import Customer
 
 
-class SignUpSerializer(serializers.ModelSerializer):
+class CreateSignUpModelSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Customer
         fields = ("username", "first_name", "last_name", "email", "password")
+
+
+class RetriveSignUpModelSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Customer
+        fields = ("first_name", "last_name", "username", "email",
+                  "date_joined", "last_login", "is_staff")
