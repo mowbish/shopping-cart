98b107b0d77ee4b9245f5c2f58e112ee94dede78
mowbish <mobin.clash.ma@gmail.com>
2022-11-24 23:35:58 +0330

    feat: Added UpdateSignUpModelSerializer

--- src/conf/accounts/api/serializers.py
+++ src/conf/accounts/api/serializers.py
@@ -15,3 +15,18 @@ class RetriveSignUpModelSerializer(serializers.ModelSerializer):
         model = Customer
         fields = ("first_name", "last_name", "username", "email",
                   "date_joined", "last_login", "is_staff")
+
+
+class UpdateSignUpModelSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Customer
+        fields = ("first_name", "last_name", "username", "email")
+
+    def validate(self, attrs):
+
+        if attrs["username"] in Customer.objects.all().exists():
+            raise serializers.ValidationError("This Username already taken")
+        elif attrs["email"] in Customer.objects.all().exists():
+            raise serializers.ValidationError("This email already taken")
+        return super().validate(attrs)
