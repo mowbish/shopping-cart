3a4a84d6c077f25837bdcfdcfdf81c63dc7ec1b1
mowbish <mobin.clash.ma@gmail.com>
2023-01-19 21:28:49 +0330

    feat: added destroy endpoint for deleting address

--- back-end/src/accounts/api/views.py
+++ back-end/src/accounts/api/views.py
@@ -1,9 +1,10 @@
 from rest_framework import viewsets, mixins
-from .permissions import IsOwner
+from .permissions import IsOwner, IsAddressOwner
 from .serializers import (SignUpUserModelSerializer,
                           RetriveUserModelSerializer, UpdateUserModelSerializer, DestroyUserModelSerializer,
                           CreateUserAddressModelSerializer, ListUserAddressModelSerializer,
-                          RetrieveUserAddressModelSerializer)
+                          RetrieveUserAddressModelSerializer, DestroyUserAddressModelSerializer, 
+                          UpdateUserAddressModelSerializer)
 from accounts.models import Customer, Address
 from rest_framework.permissions import IsAuthenticated
 
@@ -32,17 +33,20 @@ class UserAddressModelViewSet(mixins.CreateModelMixin, mixins.UpdateModelMixin,
                               mixins.DestroyModelMixin, mixins.RetrieveModelMixin,
                               mixins.ListModelMixin, viewsets.GenericViewSet):
     lookup_field = "id"
-    permission_classes = (IsAuthenticated, )
-    
+    permission_classes = (IsAuthenticated, IsAddressOwner)
+
     def get_queryset(self):
         return Address.objects.filter(customer=self.request.user)
 
     def get_serializer_class(self):
         if self.action == "create":
             self.serializer_class = CreateUserAddressModelSerializer
+        elif self.action == "update":
+            self.serializer_class = UpdateUserAddressModelSerializer
         elif self.action == "list":
             self.serializer_class = ListUserAddressModelSerializer
         elif self.action == "retrieve":
             self.serializer_class = RetrieveUserAddressModelSerializer
-
+        elif self.action == "destroy":
+            self.serializer_class = DestroyUserAddressModelSerializer
         return self.serializer_class
