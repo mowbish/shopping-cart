a3f32f820183b67fc857260de6a14bc6e5723aef
mowbish <mobin.clash.ma@gmail.com>
2023-01-26 21:40:12 +0330

    feat: Added admin panel for user and address

--- back-end/src/accounts/admin.py
+++ back-end/src/accounts/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from accounts.models import Customer, Address
 
-# Register your models here.
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ("username", "first_name", "last_name",
+                    "email", "date_joined")
+    search_fields = ('username', "first_name",
+                     "last_name", "email",)
+    empty_value_display = '-empty-'
+    ordering = ('date_joined',)
+
+
+@admin.register(Address)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ("customer", "address_name", "country", "state",
+                    "city", "address_detail", "postal_code",)
+    search_fields = ("customer__username", "address_name", "country", "state",
+                    "city", "address_detail",)
+    empty_value_display = '-empty-'
+    ordering = ('created_at',)
