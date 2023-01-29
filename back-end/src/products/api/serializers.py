431c29e97ba44215aa2347f0bc7c2dda2c768715
mowbish <mobin.clash.ma@gmail.com>
2023-01-29 12:42:14 +0330

    feat: added views and serializer for product

--- back-end/src/products/api/serializers.py
+++ back-end/src/products/api/serializers.py
@@ -1,5 +1,11 @@
 from rest_framework import serializers
-from products.models import Product
+from products.models import Product, ProductImages
+
+
+class ProductImagesModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductImages
+        fields = ("product", "images")
 
 
 class AllProductsModelSerializer(serializers.ModelSerializer):
@@ -7,3 +13,13 @@ class AllProductsModelSerializer(serializers.ModelSerializer):
         model = Product
         fields = ("name", "color", "base_image",
                   "count_of_product", "price", "category")
+
+
+class RetriveProductsModelSerializer(serializers.ModelSerializer):
+
+    product_images = ProductImagesModelSerializer(read_only=True)
+
+    class Meta:
+        model = Product
+        fields = ("name", "description", "features", "color", "base_image",
+                  "count_of_product", "price", "category", "is_active", "views", "product_images")
--- back-end/src/products/api/views.py
+++ back-end/src/products/api/views.py
@@ -1,13 +1,19 @@
-# from rest_framework import viewsets, mixins
-# from products.api.serializers import AllProductsModelSerializer
-# from products.models import Product
+from rest_framework import viewsets, mixins
+from products.api.serializers import AllProductsModelSerializer, RetriveProductsModelSerializer
+from products.models import Product
 
-# class ProductModelViewSet(
-#         mixins.ListModelMixin,
-#         # mixins.UpdateModelMixin,
-#         # mixins.RetrieveModelMixin,
-#         # mixins.DestroyModelMixin,
-#         viewsets.GenericViewSet):
-#     def get_queryset(self):
-#         return Product.objects.all()
-#     pagination_class = 
+class ProductModelViewSet(
+        mixins.ListModelMixin,
+        # mixins.UpdateModelMixin,
+        mixins.RetrieveModelMixin,
+        # mixins.DestroyModelMixin,
+        viewsets.GenericViewSet):
+    def get_queryset(self):
+        return Product.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            self.serializer_class = AllProductsModelSerializer
+        elif self.action == "retrieve":
+            self.serializer_class = RetriveProductsModelSerializer
+        return self.serializer_class
