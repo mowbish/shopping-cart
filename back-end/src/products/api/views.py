7c48d8149410d5260b4e89b35f7c61be3f90c82b
mowbish <mobin.clash.ma@gmail.com>
2023-07-19 09:55:22 +0330

    feat: Created category class

--- back-end/src/products/api/views.py
+++ back-end/src/products/api/views.py
@@ -1,11 +1,28 @@
-from rest_framework import viewsets, mixins
-from products.api.serializers import AllProductsModelSerializer, RetriveProductsModelSerializer
-from products.models import Product
+from rest_framework import viewsets, mixins, status
+from products.api.serializers import CategoryModelSerializer, AllProductsModelSerializer, RetriveProductsModelSerializer
+from products.models import Product, Category
 from django.db.models import Q
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAdminUser, AllowAny
+from rest_framework.response import Response
+
+class CategoryModelViewSet(
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin, 
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet):
+    
+    permission_classes = (IsAdminUser,)
+    serializer_class = CategoryModelSerializer
+
+    def get_queryset(self):
+        return Category.objects.all()
 
 class ProductModelViewSet(
-        mixins.ListModelMixin,
         mixins.RetrieveModelMixin,
+        mixins.ListModelMixin,
         viewsets.GenericViewSet):
     def get_queryset(self):
         return Product.objects.exclude(Q(stock_count=None) | Q(stock_count=0) | Q(extant=False))
