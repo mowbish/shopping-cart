f0ebc96ccb2d7ec116a4d4e1b8c45fb4a7a781c7
mowbish <mobin.clash.ma@gmail.com>
2023-07-21 12:01:57 +0330

    feat: added CELERY to peoject

---
+++ back-end/src/conf/celery.py
@@ -0,0 +1,8 @@
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.settings')
+
+celery_app = Celery('conf')
+celery_app.config_from_object('django.conf:settings', namespace='CELERY')
+celery_app.autodiscover_tasks()
